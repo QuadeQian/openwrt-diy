694672fb42627d5fb960736b2f27dcd72f100340
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-26 22:15:15 +0800

    Update diy-step-1.sh
    增加第三方插件的汉化包

--- docker_inside/shell/diy-step-1.sh
+++ docker_inside/shell/diy-step-1.sh
@@ -21,30 +21,25 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# Themes
-#git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon ${FEED_DIR}/luci-theme-argon
-#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config ${FEED_DIR}/luci-app-argon-config
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-theme-argon luci-app-argon-config"
-
 # 晶晨宝盒
 git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
 sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/QuadeQian/openwrt-diy'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
 # sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
 sed -i "s|ARMv8|OWRT|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 
 # iStore
 #git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui 
 #git_sparse_clone main https://github.com/linkease/istore luci 
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES app-store-ui taskd luci-lib-taskd luci-lib-xterm luci-app-store"
+#CUSTOM_PACKAGES="$CUSTOM_PACKAGES app-store-ui luci-app-store luci-i18n-store-zh-cn"
 
 # 测速插件
 git_sparse_clone lua https://github.com/sirpdboy/luci-app-netspeedtest homebox luci-app-netspeedtest
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES homebox luci-app-netspeedtest"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES homebox luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
 
 # turboacc
 git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
 
 echo "CUSTOM_PACKAGES=\"$CUSTOM_PACKAGES\"" > ./envfile/custom-packages.env
 
