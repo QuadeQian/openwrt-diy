d9ccec3343a519c7e0f6fec9cfc1d78b18126aab
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-28 19:43:15 +0800

    Update diy-step-2.sh
    空间不足，删减包

--- platform/ramips/oraybox_x3a/diy-step-2.sh
+++ platform/ramips/oraybox_x3a/diy-step-2.sh
@@ -2,8 +2,4 @@
 
 PACKAGES=""
 PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 echo "PACKAGES=\"$PACKAGES\"" >> ./envfile/custom-packages.env
