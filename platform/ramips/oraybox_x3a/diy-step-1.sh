cca9a13c712e9da92c75d340e7ade7a8c1e53617
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-28 19:42:05 +0800

    Update diy-step-1.sh
    空间不足，删减包

--- platform/ramips/oraybox_x3a/diy-step-1.sh
+++ platform/ramips/oraybox_x3a/diy-step-1.sh
@@ -35,8 +35,8 @@ function git_sparse_clone() {
 # CUSTOM_PACKAGES="$CUSTOM_PACKAGES homebox luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
 
 # turboacc
-git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
+# git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
 
 # easytier
 git_sparse_clone main https://github.com/EasyTier/luci-app-easytier easytier luci-app-easytier
