8083a64ffefb049012bdaf94357d8ee280b8b4c0
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-28 19:44:01 +0800

    Update diy-step-1.sh

--- platform/ramips/oraybox_x3a/diy-step-1.sh
+++ platform/ramips/oraybox_x3a/diy-step-1.sh
@@ -18,28 +18,8 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# 晶晨宝盒
-# git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
-# sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/QuadeQian/openwrt-diy'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
-# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
-# sed -i "s|ARMv8|OWRT-$PROFILE|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
-# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
-
-# iStore
-#git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui 
-#git_sparse_clone main https://github.com/linkease/istore luci 
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES app-store-ui luci-app-store luci-i18n-store-zh-cn"
-
-# 测速插件
-# git_sparse_clone lua https://github.com/sirpdboy/luci-app-netspeedtest homebox luci-app-netspeedtest
-# CUSTOM_PACKAGES="$CUSTOM_PACKAGES homebox luci-app-netspeedtest luci-i18n-netspeedtest-zh-cn"
-
-# turboacc
-# git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
-# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
-
 # easytier
 git_sparse_clone main https://github.com/EasyTier/luci-app-easytier easytier luci-app-easytier
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier luci-i18n-easytier-zh-cn"
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
 
 echo "CUSTOM_PACKAGES=\"$CUSTOM_PACKAGES\"" > ./envfile/custom-packages.env
