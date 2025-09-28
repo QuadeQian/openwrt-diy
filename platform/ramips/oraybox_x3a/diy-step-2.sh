c21b67197a2d4931d7de1126ab7c35da0ab3695d
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-28 17:29:33 +0800

    Create diy-step-2.sh
    add oraybox_x3a

---
+++ platform/ramips/oraybox_x3a/diy-step-2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PACKAGES=""
+PACKAGES="$PACKAGES luci-theme-argon"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+echo "PACKAGES=\"$PACKAGES\"" >> ./envfile/custom-packages.env
