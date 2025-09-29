cae083afdbf6dcb327020c2914a20796cec05c3f
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-29 09:20:47 +0800

    Update diy-step-2.sh
    增加zerotier和wireguard

--- platform/ramips/oraybox_x3a/diy-step-2.sh
+++ platform/ramips/oraybox_x3a/diy-step-2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-PACKAGES="base-files block-mount ca-bundle default-settings-chn dnsmasq-full dropbear firewall4 fstools"
-PACKAGES="$PACKAGES kmod-crypto-hw-eip93 kmod-gpio-button-hotplug kmod-leds-gpio kmod-nf-nathelper kmod-nf-nathelper-extra kmod-nft-offload"
-PACKAGES="$PACKAGES libc libgcc libustream-openssl logd luci-app-package-manager luci-compat luci-lib-base luci-lib-ipkg luci-light"
-PACKAGES="$PACKAGES mtd netifd nftables odhcp6c odhcpd-ipv6only opkg ppp ppp-mod-pppoe procd-ujail uboot-envtools uci uclient-fetch"
-PACKAGES="$PACKAGES urandom-seed urngd wpad-openssl kmod-mt7615-firmware -uboot-envtools"
+PACKAGES=""
 PACKAGES="$PACKAGES luci-theme-argon"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+PACKAGES="$PACKAGES luci-proto-wireguard"
 echo "PACKAGES=\"$PACKAGES\"" >> ./envfile/custom-packages.env
