6cfa79a107fb859510bba24ca68d4b2b47658dd5
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-09-29 09:06:59 +0800

    Update diy-step-1.sh
    remove easytier

--- platform/ramips/oraybox_x3a/diy-step-1.sh
+++ platform/ramips/oraybox_x3a/diy-step-1.sh
@@ -18,8 +18,8 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# easytier
-git_sparse_clone main https://github.com/EasyTier/luci-app-easytier easytier luci-app-easytier
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES easytier luci-app-easytier"
+# turboacc
+git_sparse_clone luci https://github.com/chenmozhijin/turboacc luci-app-turboacc
+CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-turboacc luci-i18n-turboacc-zh-cn"
 
 echo "CUSTOM_PACKAGES=\"$CUSTOM_PACKAGES\"" > ./envfile/custom-packages.env
