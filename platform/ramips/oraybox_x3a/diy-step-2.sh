1a436e1edd50e65b80d211e543ab673097d88e8d
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-10-10 14:53:45 +0800

    Update diy-step-2.sh
    删除其他异地组网包

--- platform/ramips/oraybox_x3a/diy-step-2.sh
+++ platform/ramips/oraybox_x3a/diy-step-2.sh
@@ -6,6 +6,4 @@ PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
-PACKAGES="$PACKAGES luci-proto-wireguard"
 echo "PACKAGES=\"$PACKAGES\"" >> ./envfile/custom-packages.env
