f353f86558780aa837cc506b182b5af4afe6bb9e
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-10-10 19:14:46 +0800

    Update diy-step-2.sh
    备选组网工具 zerotier

--- platform/ramips/oraybox_x3a/diy-step-2.sh
+++ platform/ramips/oraybox_x3a/diy-step-2.sh
@@ -6,4 +6,5 @@ PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
 echo "PACKAGES=\"$PACKAGES\"" >> ./envfile/custom-packages.env
