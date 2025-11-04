c17186e44a9971fb242ebe4f84ef7ecd86d05fd0
QuadeQian <86300780+QuadeQian@users.noreply.github.com>
2025-11-04 11:43:27 +0800

    Update diy-step-1.sh
    移除晶晨宝盒

--- platform/rockchip/lunzn_fastrhino-r68s/diy-step-1.sh
+++ platform/rockchip/lunzn_fastrhino-r68s/diy-step-1.sh
@@ -19,16 +19,16 @@ function git_sparse_clone() {
 }
 
 # 晶晨宝盒
-git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
-sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/QuadeQian/openwrt-diy'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
+# git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
+# sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/QuadeQian/openwrt-diy'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
 # sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|ARMv8|OWRT-$PROFILE|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
-CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
+# sed -i "s|ARMv8|OWRT-$PROFILE|g" ${FEED_DIR}/luci-app-amlogic/root/etc/config/amlogic
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 
 # iStore
-#git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui 
-#git_sparse_clone main https://github.com/linkease/istore luci 
-#CUSTOM_PACKAGES="$CUSTOM_PACKAGES app-store-ui luci-app-store luci-i18n-store-zh-cn"
+# git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui 
+# git_sparse_clone main https://github.com/linkease/istore luci 
+# CUSTOM_PACKAGES="$CUSTOM_PACKAGES app-store-ui luci-app-store luci-i18n-store-zh-cn"
 
 # 测速插件
 git_sparse_clone js https://github.com/sirpdboy/luci-app-netspeedtest homebox netspeedtest luci-app-netspeedtest
